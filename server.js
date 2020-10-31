d325e0972a3f8fb0d59de3efbffdfe3074023321
Hiba-moh <Hiba.moh@yahoo.com>
2020-10-31 15:51:58 +0000

    first heroku

--- server.js
+++ server.js
@@ -1,14 +1,14 @@
-const express = require("express");
-const cors = require("cors");
+const express = require ('express');
+const cors = require ('cors');
 
-const app = express();
+const app = express ();
 
-app.use(cors());
+app.use (cors ());
 
 const welcomeMessage = {
   id: 0,
-  from: "Bart",
-  text: "Welcome to CYF chat system!",
+  from: 'Bart',
+  text: 'Welcome to CYF chat system!',
 };
 
 //This array is our "data store".
@@ -16,8 +16,12 @@ const welcomeMessage = {
 //Note: messages will be lost when Glitch restarts our server.
 const messages = [welcomeMessage];
 
-app.get("/", function (request, response) {
-  response.sendFile(__dirname + "/index.html");
+app.get ('/', function (request, response) {
+  response.sendFile (__dirname + '/index.html');
 });
 
+
 app.listen(process.env.PORT);
+// const listener = app.listen (process.env.PORT || 3000, function () {
+//   console.log ('Your app is listening on port ' + listener.address ().port);
+// });
